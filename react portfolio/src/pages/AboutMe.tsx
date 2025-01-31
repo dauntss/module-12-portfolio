dcda845f49d93ae9591360e0df1a2bdc877faee2
dauntss <kat.krahulec@gmail.com>
2025-01-30 20:35:10 -0700

    added profile photo

--- react portfolio/src/pages/AboutMe.tsx
+++ react portfolio/src/pages/AboutMe.tsx
@@ -2,8 +2,9 @@ const AboutMe = () => {
     return (
         <>
           <h2>About Me</h2>
+          <img src="./dante.jpg" alt="Profile picture" />
           <p>
-            I am a full stack web developer with a passion for creating dynamic and responsive web applications. I have experience with HTML, CSS, JavaScript, and React on the front end, and Node.js, Express, and MongoDB on the back end. I am a quick learner and a team player, and I am always looking for new challenges and opportunities to grow.
+            My name is Dante, I live in S alt Lake City and I use she/her pronouns. I am a full stack web developer with a passion for creating dynamic and responsive web applications. I have experience with HTML, CSS, JavaScript, and React on the front end, and Node.js, Express, and MongoDB on the back end. I am a quick learner and a team player, and I am always looking for new challenges and opportunities to grow.
           </p>
         </>
     );
